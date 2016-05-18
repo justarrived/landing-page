7bc3b4b73ab62bd91ce6a770f65bf671070b4446
David <david.mccracken@nova.com>
2016-05-18 09:07:29 +0200

    New modules added

--- wp-content/themes/JustArrived/tpl-homepage.php
+++ wp-content/themes/JustArrived/tpl-homepage.php
@@ -27,7 +27,7 @@
 					<div class="wrap">
 						<div class="
 						module-content">
-							<h2>Job opportunities for new arrivals</h2>
+							<h2 class="bordered bordered--primary module--txt-c">Job opportunities for new arrivals</h2>
 							<div class="columns">
 								<div class="column-1-2">
 									<p>We connect newly-arrived immigrants with Swedish businesses who need help with day-to-day activities. Our digital platform enables businesses to post simple tasks and services, and match them with new arrivals looking for work.</p>
@@ -43,13 +43,13 @@
 
 				<section class="
 					section--how-to
-					module--txt-c
 					module
 					module--padding
-					module--black">
+					module--black
+					module--txt-c">
 					<div class="wrap">
 						<div class="module-content">
-							<h2>JustArrived makes it simple for companies to engage or employ a new arrival.</h2>
+							<h2 class="bordered">JustArrived makes it simple for companies to engage or employ a new arrival.</h2>
 
 							<div class="columns">
 								<div class="column-1-3">
@@ -74,20 +74,41 @@
 				</section>
 
 				<section class="
+						section--product
+						module
+						module--padding
+						module--txt-img
+						module--white
+						module--charcoal-bg">
+						<div class="wrap">
+							<div class="product-image">
+								<img src="<?php echo get_template_directory_uri(); ?>/library/images/inline/iphone6.png">
+							</div>
+
+							<div class="product-summary
+								">
+								<h2 class="bordered bordered--white bordered--left">Our Product</h2>
+								<p>In 2015, more than 160,000 people applied for asylum in Sweden. These are people like you and me, with the same needs and desires. However, statistics show that it takes many years for most new arrivals to get a job and support themselves in Sweden.*</p>
+							</div>
+						</div>
+				</section>
+
+				<section class="
 						section--change
 						module
-						module--txt-img">
-						<div class="wrap">
+						module--txt-img
+						module--white
+						module--secondary-bg">
 
-							<div class="module--img"
+							<div class="module--img module--img-left"
 								style="background-image: url(<?php echo get_template_directory_uri(); ?>/library/images/bgs/beach.jpg); background-position: 50% 50%;">
 							</div>
 
 							<div class="
 								module--txt
-								module--primary
-								module--primary-bg">
-								<h2>Be the change</h2>
+								module--txt-right
+								">
+								<h2 class="bordered bordered--white bordered--left">Be the change</h2>
 								<p>In 2015, more than 160,000 people applied for asylum in Sweden. These are people like you and me, with the same needs and desires. However, statistics show that it takes many years for most new arrivals to get a job and support themselves in Sweden.*</p>
 		 
 								<p><strong> Now, Swedish businesses have the opportunity to make a difference.</strong></p>
@@ -97,34 +118,20 @@
 								<p><small><em>* After ten years, only 60% of all immigrants have a job by which they can support themselves. http://forte.se/artikel/svarforcerad-arbetsmarknad-for-nyanlanda </small> </em></p>
 							</div>
 
-							
-
-						</wrap>
-
 				</section>
 
-
-				<section class="
-					section--meet-team
-					module
-					module--padding
-					module--white
-					module--bg-cover"
-					style="background-image: url(<?php echo get_template_directory_uri(); ?>/library/images/bgs/couple.jpg)">
-				</section>
 
 				<section class="
 					module--meet-team-text
 					module--padding
-					module--white
-					module--black-bg">
+					module--black">
 					<div class="wrap">
 						<div class="
 						module-content
 						module-content--centered
 						module--txt-c
 						">
-							<h2>Meet the team</h2>
+							<h2 class="bordered bordered--primary">Meet the team</h2>
 							<div class="module--txt-l">
 								<p>We’re a group of 80 volunteers, working evenings and weekends to make JustArrived a reality.</p>
 								<p>We collaborate with a number of organisations. Antrop, Jacob Burenstam, Cygni and NeverEndingMonday are currently developing our product. A team comprised of people from Wintrgarden, Universum, RödaKorset and Nova is managing other areas. Our project manager is Andreas König from Wintrgarden.</p>
@@ -132,6 +139,16 @@
 						</div>
 					</div>
 				</section>
+
+				<section class="
+					section--meet-team
+					module
+					module--padding
+					module--white
+					module--bg-cover"
+					style="background-image: url(<?php echo get_template_directory_uri(); ?>/library/images/bgs/team.jpg)">
+				</section>
+
 
 				<!-- <section class="
 					module
@@ -153,24 +170,23 @@
 				</section> -->
 
 				<section class="
+					section--joinus
 					module
-					module--joinus
 					module--padding
-					module--white
-					module--secondary-bg">
+					module--black">
 					<div class="wrap">
 						<div class="
 						module-content
 						module-content--centered
-						module--txt-c
 						">
-							<h2>Join us</h2>
-							<div class="module--txt-l">
+							<h2 class="bordered bordered--secondary module--txt-c">Join us</h2>
+							<div>
 								<p>JustArrived is driven by a team of volunteers that wants to make a difference. If you think you can help in any way, please <a class="txt-link" href="hej@justarrived.se">get in touch</a>.</p>
  
-							<p>We're currently looking for programmers (AngularJS, Ruby on Rails), translators and sponsors. If you're a developer, we’d love you to contribute to our GitHub repository.</p>
-							<br>
-							<a href="#" class="border-btn transparent-btn--blue btn-large">Join us</a>
+							<p>We're currently looking for programmers (AngularJS, Ruby on Rails), translators and sponsors. If you're a developer, we’d love you to contribute to our GitHub repository.<br><br></p>
+							<p class="module--txt-c">
+								<a href="#" class="border-btn border-btn--blue btn-large">Join us</a>
+							</p>
 							</div>
 						</div>
 					</div>
@@ -178,35 +194,36 @@
 
 				<section class="
 					section--say-hej
-					module--txt-c
 					module
+					module--txt-c
 					module--padding
-					module--black">
+					module--white
+					module--black-bg">
 					<div class="wrap">
 						<div class="module-content">
-							<h2>Say hej</h2>
+							<h2 class="bordered bordered--secondary bordered--centered">Say hej</h2>
 
-							<div class="columns">
+							<div class="columns columns--inline">
 								<div class="column-1-3">
-									<img class="profile-pic" src="http://placehold.it/80x80" width="80" height="80">
+									<img class="profile-pic" src="<?php echo get_template_directory_uri(); ?>/library/images/profile-pictures/pp.jpg">
 									<p>
-										General enquiries <br>
+										<strong>General enquiries</strong> <br>
 										<a class="txt-link" href="#">hej@justarrived.se</a> <br>
 										<a class="txt-link" href="tel:+46 733 868656">+46 733 868656</a>
 									</p>
 								</div>
 								<div class="column-1-3">
-									<img class="profile-pic" src="http://placehold.it/80x80" width="80" height="80">
+									<img class="profile-pic" src="<?php echo get_template_directory_uri(); ?>/library/images/profile-pictures/pp1.jpg">
 									<p>
-										Andreas König <br>
+										<strong>Andreas König</strong> <br>
 										Project Manager <br>
 										<a class="txt-link" href="mailto:andreas@justarrived.se">andreas@justarrived.se</a>
 									</p>
 								</div>
 								<div class="column-1-3">
-									<img class="profile-pic" src="http://placehold.it/80x80" width="80" height="80">
+									<img class="profile-pic" src="<?php echo get_template_directory_uri(); ?>/library/images/profile-pictures/pp2.jpg">
 									<p>
-										Fredric Ghatan <br>
+										<strong>Fredric Ghatan</strong> <br>
 										Product Development <br>
 										<a class="txt-link" href="mailto:fredric@justarrived.se">fredric@justarrived.se</a>
 									</p>
@@ -218,35 +235,76 @@
 				</section>
 
 				<section class="
+					section--mailinglist
 					module
-					module--malinglist
 					module--padding
-					module--white
-					module--primary-bg">
+					module--black">
 					<div class="wrap">
 						<div class="
 						module-content
 						module-content--centered
 						module--txt-c
 						">
-							<h2>Join our mailing list to find out more</h2>
+							<h2 class="bordered bordered--primary">Join our mailing list to find out more</h2>
 							<form class="signup-form">
-							<div class="form-col">
-								<input type="text" placeholder="Name" />
+								<div class="form-col">
+									<input type="text" placeholder="Name" />
+								</div>
+								<div class="form-col">
+									<input type="email" placeholder="Email" />
+								</div>
+								<div class="form-col">
+									<input type="number" placeholder="Phone" />
+								</div>
+								<div class="form-col">
+									<input type="text" placeholder="Company" />
+								</div>
+								<div class="form-col form-col--submit-btn">
+									<input type="submit" value="Sign up" class="border-btn btn-large border-btn--pink">
+								</div>
+							</form>
+						</div>
+					</div>
+				</section>
+
+				<section class="
+					section--companies
+					module
+					module--padding
+					module--black">
+					<div class="wrap">
+						<div class="
+						module-content
+						module-content--centered
+						module--txt-c
+						">
+							<h2 class="bordered bordered--primary">We've collaborated with some great companies</h2>
+							<div class="companies">
+								<div class="company">
+									<img src="http://placehold.it/100x60">
+								</div>
+								<div class="company">
+									<img src="http://placehold.it/100x60">
+								</div>
+								<div class="company">
+									<img src="http://placehold.it/100x60">
+								</div>
+								<div class="company">
+									<img src="http://placehold.it/100x60">
+								</div>
+								<div class="company">
+									<img src="http://placehold.it/100x60">
+								</div>
+								<div class="company">
+									<img src="http://placehold.it/100x60">
+								</div>
+								<div class="company">
+									<img src="http://placehold.it/100x60">
+								</div>
+								<div class="company">
+									<img src="http://placehold.it/100x60">
+								</div>
 							</div>
-							<div class="form-col">
-								<input type="email" placeholder="Email" />
-							</div>
-							<div class="form-col">
-								<input type="number" placeholder="Phone" />
-							</div>
-							<div class="form-col">
-								<input type="text" placeholder="Company" />
-							</div>
-							<div class="form-col form-col--submit-btn">
-								<input type="submit" value="Sign up" class="border-btn">
-							</div>
-						</form>
 						</div>
 					</div>
 				</section>
