fb51b5cc820ffec796359e34ee99ab27a32716c8
David McCracken <david.mccracken@admins-iMac.local>
2016-06-28 17:39:13 +0200

    Added the Almedalen template

---
+++ wp-content/themes/JustArrived/tpl-almadalen.php
@@ -0,0 +1,110 @@
+<?php
+/*
+ Template Name: Almedalen
+ *
+ * This is your custom page template. You can create as many of these as you need.
+ * Simply name is "page-whatever.php" and in add the "Template Name" title at the
+ * top, the same way it is here.
+ *
+ * When you create your page, you can just select the template and viola, you have
+ * a custom page template to call your very own. Your mother would be so proud.
+ *
+ * For more info: http://codex.wordpress.org/Page_Templates
+*/
+?>
+
+<?php get_header(); ?>
+			
+
+
+					<div class="almedalen-logo"></div>
+
+					<div class="almedalen-content-container">
+
+						<div class="almedalen-signup">
+
+							<section class="section--signup-popup">
+								<div class="
+								module-content
+								module-content--centered
+								module--black
+								">
+									<h2 class="bordered bordered--primary module--txt-c">Sign up</h2>
+									<p>We are currently testing the platform (May - September) with an exclusive group of companies and new arrivals. If you are interested in utilizing the platform after our official launch on the 1st of September, please fill in the form and we will get back to you!</p>
+									<div id="mc_embed_signup">
+										<form action="//justarrived.us13.list-manage.com/subscribe/post?u=ac005505ee4f55f35fc7bdc5d&amp;id=4cc4f7c8c5" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate signup-form" target="_blank" novalidate>
+
+										<div class="signup-error module--txt-c">
+											<label for="mce-EMAIL" class="mailchimp-error">Email Address</label>
+										</div>
+
+										    <div id="mc_embed_signup_scroll">
+												
+												<div class="form-col mc-field-group">
+													<label>Email Address <span class="asterisk">*</span></label>
+													<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+												</div>
+												<div class="form-col mc-field-group">
+													<label for="mce-NAME">Name <span class="asterisk">*</span>
+												</label>
+													<input type="text" value="" name="NAME" class="required" id="mce-NAME">
+												</div>
+												<div class="form-col mc-field-group size1of2">
+													<label for="mce-PHONE">Phone Number</label>
+													<input type="number" name="PHONE" class="" value="" id="mce-PHONE">
+												</div>
+												<div class="form-col mc-field-group">
+													<label for="mce-COMPANY">Company</label>
+													<input type="text" value="" name="COMPANY" class="" id="mce-COMPANY">
+												</div>
+												<div class="form-col form-col--full-width mc-field-group">
+													<label for="mce-LOCATION">Location</label>
+													<input type="text" value="" name="LOCATION" class="" id="mce-LOCATION">
+												</div>
+												<div class="mc-field-group input-group">
+													<div class="input-group-content">
+														<p>I'm interested in...</p>
+														<ul>
+															<li><input type="radio" value="Posting jobs" name="MMERGE5" id="mce-MMERGE5-0"><label for="mce-MMERGE5-0">Posting jobs</label></li>
+															<li><input type="radio" value="Finding jobs" name="MMERGE5" id="mce-MMERGE5-1"><label for="mce-MMERGE5-1">Finding jobs</label></li>
+															<li><input type="radio" value="Helping out" name="MMERGE5" id="mce-MMERGE5-2"><label for="mce-MMERGE5-2">Helping out</label></li>
+															<li><input type="radio" value="Donating" name="MMERGE5" id="mce-MMERGE5-3"><label for="mce-MMERGE5-3">Donating</label></li>
+															<li><input type="radio" value="Knowing more" name="MMERGE5" id="mce-MMERGE5-4"><label for="mce-MMERGE5-4">Knowing more</label></li>
+														</ul>
+													</div>
+												</div>
+												<div id="mce-responses" class="clear">
+													<div class="response" id="mce-error-response" style="display:none"></div>
+													<div class="response" id="mce-success-response" style="display:none"></div>
+												</div>
+											    <div style="position: absolute; left: -5000px;" aria-hidden="true">
+											    	<input type="text" name="b_ac005505ee4f55f35fc7bdc5d_4cc4f7c8c5" tabindex="-1" value="">
+											    </div>
+										    	<div class="clear">
+										    		<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button border-btn btn-large border-btn--pink">
+										    	</div>
+										    </div>
+										</form>
+									</div>
+								</div>
+								<a href="#0" class="cd-popup-close img-replace"></a>
+							</section>
+
+						</div> <!-- /almedalen-signup --> 
+
+						<div class="almedalen-copy">
+
+							<h1>Om Just Arrived</h1>
+							<p>avgörande skillnad för alla de nyanlända som kommer till Sverige. Just Arrived skulle drivas som ett icke vinstdrivande företag och ligga under en stiftelse. Initiativet skulle dessutom byggas upp pro bono, av människor och företag som frivilligt ger av sin tid, pengar och sitt engagemang. Idag engagerar Just Arrived över 100 personer som representerar 20 olika företag och myndigheter i Sverige.</p>
+							<p>Tillsammans har vi lagt tusentals timmar på att ta fram den webb-applikation som matchar nyanlända med jobbmöjligheter. Vi tar fullt arbetsgivaransvar genom att betala skatter, sociala avgifter och försäkringar för de nyanlända. Detta innebär minimal administration för de företag som lägger upp uppdrag på vår sida. Framför allt för Just Arrived ihop två grupper i samhället som letar efter varandra; företag som vill hjälpa nyanlända samt nyanlända som letar efter jobb.</p>
+
+						</div> <!-- /almedalen-copy -->
+
+					</div>
+				
+
+				
+
+
+
+<?php get_footer(); ?>
