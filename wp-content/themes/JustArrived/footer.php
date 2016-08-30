13680907760e7269f884c2891b38cbcea12e6122
David McCracken <david.mccracken@nova.com>
2016-08-30 17:33:32 +0200

    Added LinkedIn tracking code to footer

--- wp-content/themes/JustArrived/footer.php
+++ wp-content/themes/JustArrived/footer.php
@@ -27,10 +27,15 @@
 		<?php // all js scripts are loaded in library/bones.php ?>
 		<?php wp_footer(); ?>
 
-		
-
-		
-
+		<script type="text/javascript">
+			_linkedin_data_partner_id = "17815";
+			</script><script type="text/javascript">
+			(function(){var s = document.getElementsByTagName("script")[0];
+			var b = document.createElement("script");
+			b.type = "text/javascript";b.async = true;
+			b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+			s.parentNode.insertBefore(b, s);})();
+		</script>
 	</body>
 
 </html> <!-- end of site. what a ride! -->
